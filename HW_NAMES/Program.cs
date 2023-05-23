f3e518d6160dbf538aba88b5eba1ab7be1072665
VadimGera <vadimnvidia@gmail.com>
2023-05-24 00:26:59 +0300

    ДЗ

---
+++ HW_NAMES/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "James", "John", "Robert", "Michael", "William", "Charles", "Joseph", "Thomas" };
+
+        var namesEndingWithS = from name in names
+            where name.EndsWith("s")
+            select name;
+
+        Console.WriteLine("Имена, заканчивающиеся на 'S':");
+        foreach (var name in namesEndingWithS)
+        {
+            Console.WriteLine(name);
+        }
+
+        int maxLength = names.Max(name => name.Length);
+
+        Console.WriteLine("Размер самого длинного имени: " + maxLength);
+    }
+}
